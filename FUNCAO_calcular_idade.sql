d099f017679d10cef84bc9485f877409325edf33
Jeferson Alan <cabello_10@live.com>
2024-09-18 21:37:15 -0300

    Update FUNCAO_calcular_idade.sql

--- FUNCAO_calcular_idade.sql
+++ FUNCAO_calcular_idade.sql
@@ -16,3 +16,38 @@ SELECT nome, calcular_idade(data_nascimento) AS idade FROM clientes
     
 SELECT nome, calcular_idade(data_nascimento) AS idade FROM clientes
 	WHERE calcular_idade(data_nascimento) >= 18 ;
+    
+-- CRIE UMA FUNCAO PARA CONTAR OS ENDERECOS
+select COUNT(endereco) from enderecos;
+
+-- FUNCAO CONTANDO CADA ID 
+DELIMITER //
+CREATE FUNCTION contar_endereco(endereco VARCHAR(30)) RETURNS INT
+DETERMINISTIC
+	BEGIN
+		DECLARE Total INT;
+		SELECT COUNT(*) INTO Total FROM enderecos;
+		RETURN Total;
+	END //
+DELIMITER ;
+
+SELECT DISTINCT contar_endereco(endereco) AS total_enderecos FROM enderecos;
+
+-- FUNCAO CONTADO TUDO SEM PRECISAR CONTAR CADA ID
+DELIMITER //
+CREATE FUNCTION contar_endereco1() RETURNS INT
+DETERMINISTIC
+	BEGIN
+		DECLARE Total INT;
+		SELECT COUNT(*) INTO Total FROM enderecos;
+		RETURN Total;
+	END //
+DELIMITER ;
+
+
+SELECT contar_endereco1();
+
+DROP FUNCTION contar_endereco;
+
+
+-- CRIE UMA FUNCAO QUE RETORNA A DATA DA ULTIMA COMPRA DE UM CLIENTE
